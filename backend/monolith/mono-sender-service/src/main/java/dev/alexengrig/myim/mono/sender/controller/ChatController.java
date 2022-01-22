07087a1c6ca9be551588cef0e0ba5870060bdbd7
Grig Alex <alexengrigdev@gmail.com>
2022-01-22 04:38:15 +0300

    Fix duplicate bean name 'chatController'
    Annotation-specified bean name 'chatController' for bean class [dev.alexengrig.myim.mono.sender.controller.ChatController] conflicts with existing, non-compatible bean definition of same name and class [dev.alexengrig.myim.mono.recipient.controller.ChatController]

--- backend/monolith/mono-sender-service/src/main/java/dev/alexengrig/myim/mono/sender/controller/ChatController.java
+++ backend/monolith/mono-sender-service/src/main/java/dev/alexengrig/myim/mono/sender/controller/ChatController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RestController
+@RestController("chatSenderController")
 @RequestMapping("/api/v1/sender/chats")
 public class ChatController {
 
