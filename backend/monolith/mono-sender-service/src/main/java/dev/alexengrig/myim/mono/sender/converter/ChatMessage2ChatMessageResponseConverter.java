cb054d72d6b9fa2458614c135f24603675b0f6f6
Grig Alex <alexengrigdev@gmail.com>
2022-01-22 06:29:10 +0300

    Create ChatMessage2ChatMessageResponseConverter.java and ChatMessageSearchResult2ChatMessageSearchResponseConverter.java

---
+++ backend/monolith/mono-sender-service/src/main/java/dev/alexengrig/myim/mono/sender/converter/ChatMessage2ChatMessageResponseConverter.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2022 Alexengrig Dev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.alexengrig.myim.mono.sender.converter;
+
+import dev.alexengrig.myim.mono.sender.domain.ChatMessage;
+import dev.alexengrig.myim.mono.sender.payload.ChatMessageResponse;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ChatMessage2ChatMessageResponseConverter
+        implements Converter<ChatMessage, ChatMessageResponse> {
+
+    @Override
+    public ChatMessageResponse convert(ChatMessage source) {
+        return ChatMessageResponse.builder()
+                .text(source.getText())
+                .chatId(source.getChatId())
+                .build();
+    }
+
+}
