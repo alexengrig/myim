0279e3b1c69834f794feba74aeb56a8862f1215f
Grig Alex <alexengrigdev@gmail.com>
2022-01-26 23:41:18 +0300

    Add chat and chat list to MyimApplication.jsx

--- frontend/remyim/src/MyimApplication.jsx
+++ frontend/remyim/src/MyimApplication.jsx
@@ -14,10 +14,77 @@
  * limitations under the License.
  */
 
+import { useState } from 'react'
+import { Chat, ChatList, NoChat, NoChatList } from './components'
+
+const initChats = [
+  { id: '1', name: 'ChatItem #1' },
+  { id: '2', name: 'ChatItem #2' },
+  { id: '3', name: 'ChatItem #3' },
+  { id: '4', name: 'ChatItem #4' },
+]
+
+const initMessagesByChatId = {
+  '1': [
+    { id: '10', text: 'Message #10', author: { id: '2', name: 'User #2' } },
+    { id: '11', text: 'Message #11', author: { id: '1', name: 'User #1' } },
+  ],
+  '2': [
+    { id: '20', text: 'Message #20', author: { id: '1', name: 'User #1' } },
+    { id: '21', text: 'Message #21', author: { id: '3', name: 'User #3' } },
+  ],
+  '3': [
+    { id: '30', text: 'Message #30', author: { id: '3', name: 'User #3' } },
+    { id: '31', text: 'Message #31', author: { id: '2', name: 'User #2' } },
+  ],
+  '4': []
+}
+
 const MyimApplication = () => {
+  const [chats] = useState(initChats)
+  const [chat, setChat] = useState(null)
+  const [messagesByChatId, setMessagesByChatId] = useState(initMessagesByChatId)
+  const handleChatClick = chatId => {
+    const newChat = chats.find(chat => chat.id === chatId)
+    setChat({
+      id: newChat.id,
+      name: newChat.name,
+      messages: messagesByChatId[newChat.id]
+    })
+  }
+  const handleSend = (chatId, text) => {
+    const newMessage = {
+      text,
+      author: {
+        id: '1',
+        name: 'User #1'
+      }
+    }
+    setChat({
+      ...chat,
+      messages: [...chat.messages, newMessage]
+    })
+    setMessagesByChatId({
+      ...messagesByChatId,
+      [chatId]: [...messagesByChatId[chatId], newMessage]
+    })
+  }
   return (
     <div>
       <h1>myim</h1>
+      {(chats && chats.length) ?
+        <ChatList
+          value={chats}
+          selectedId={chat && chat.id}
+          onChatClick={handleChatClick}
+        /> :
+        <NoChatList/>}
+      {chat ?
+        <Chat
+          value={chat}
+          onSend={handleSend}
+        /> :
+        <NoChat/>}
     </div>
   )
 }
