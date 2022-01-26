73fc2594aad2f8af8e04ae94a71bb9dfdfc77edb
Grig Alex <alexengrigdev@gmail.com>
2022-01-26 23:36:35 +0300

    Create index.js for components

---
+++ frontend/remyim/src/components/index.js
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2022 Alexengrig Dev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export { default as Chat, ChatPropTypes } from './Chat'
+export { default as ChatItem, ChatItemPropTypes } from './ChatItem'
+export { default as ChatList, ChatListPropTypes } from './ChatList'
+export { default as ChatMessageInput, ChatMessageInputPropTypes } from './ChatMessageInput'
+export { default as ChatMessageItem, ChatMessageItemPropTypes } from './ChatMessageItem'
+export { default as ChatMessageList, ChatMessageListPropTypes } from './ChatMessageList'
+export { default as NoChat } from './NoChat'
+export { default as NoChatList } from './NoChatList'
+export { default as NoChatMessageList } from './NoChatMessageList'
