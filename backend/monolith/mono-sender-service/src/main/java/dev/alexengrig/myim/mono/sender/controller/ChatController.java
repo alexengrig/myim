85beb4439796daa897922eb25857773ed8c1994f
Grig Alex <alexengrigdev@gmail.com>
2022-01-29 02:48:54 +0300

    Remove CrossOrigin from ChatController

--- backend/monolith/mono-sender-service/src/main/java/dev/alexengrig/myim/mono/sender/controller/ChatController.java
+++ backend/monolith/mono-sender-service/src/main/java/dev/alexengrig/myim/mono/sender/controller/ChatController.java
@@ -27,13 +27,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController("chatSenderController")
 @RequestMapping("/api/v1/sender/chats")
 @RequiredArgsConstructor
-@CrossOrigin("*")
 public class ChatController {
 
     private static final String DEFAULT_CHATS_SIZE = "10";
