270e12f8a33a91909d4dcdab49e8571d39399af2
Grig Alex <alexengrigdev@gmail.com>
2022-02-03 03:01:12 +0300

    Replace SecurityTemplateController.java with ApplicationWebMvcConfiguration.java

--- backend/monolith/mono-security-service/src/main/java/dev/alexengrig/myim/mono/security/config/ApplicationWebMvcConfiguration.java
+++ backend/monolith/mono-security-service/src/main/java/dev/alexengrig/myim/mono/security/config/ApplicationWebMvcConfiguration.java
@@ -14,19 +14,18 @@
  * limitations under the License.
  */
 
-package dev.alexengrig.myim.mono.security.controller;
+package dev.alexengrig.myim.mono.security.config;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Controller
-@RequestMapping
-public class SecurityTemplateController {
+@Configuration
+public class ApplicationWebMvcConfiguration implements WebMvcConfigurer {
 
-    @GetMapping("/login")
-    public String login() {
-        return "login";
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
     }
 
 }
