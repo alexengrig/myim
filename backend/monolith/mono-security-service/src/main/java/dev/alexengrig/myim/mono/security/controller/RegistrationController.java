b4a7c387f9a4cdf986c7d9bd80fbe45d7c053ecc
Grig Alex <alexengrigdev@gmail.com>
2022-02-03 05:24:02 +0300

    Add validation to RegistrationController

--- backend/monolith/mono-security-service/src/main/java/dev/alexengrig/myim/mono/security/controller/RegistrationController.java
+++ backend/monolith/mono-security-service/src/main/java/dev/alexengrig/myim/mono/security/controller/RegistrationController.java
@@ -25,10 +25,13 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -46,8 +49,14 @@ public class RegistrationController {
     }
 
     @PostMapping
-    public String register(@ModelAttribute("user") UserRegistrationRequest request, Model model) {
+    public String register(
+            @Valid @ModelAttribute("user") UserRegistrationRequest request,
+            BindingResult result,
+            Model model) {
         log.info("Register: {}", request);
+        if (result.hasErrors()) {
+            return "registration";
+        }
         UserDetails user = conversionService.convert(request, UserDetails.class);
         try {
             userDetailsManager.createUser(user);
