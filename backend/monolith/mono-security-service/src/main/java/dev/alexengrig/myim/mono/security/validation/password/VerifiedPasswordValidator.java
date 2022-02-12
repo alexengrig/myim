c5784412d44b1f6e21cb092a17f0533379e663ac
Grig Alex <alexengrigdev@gmail.com>
2022-02-12 21:38:22 +0300

    Create custom validator for VerifiedPassword

---
+++ backend/monolith/mono-security-service/src/main/java/dev/alexengrig/myim/mono/security/validation/password/VerifiedPasswordValidator.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2022 Alexengrig Dev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.alexengrig.myim.mono.security.validation.password;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class VerifiedPasswordValidator
+        implements ConstraintValidator<VerifiedPassword, CharSequence> {
+
+    private final List<PasswordRequirement> requirements;
+
+    @Override
+    public boolean isValid(CharSequence value, ConstraintValidatorContext context) {
+        boolean isValid = true;
+        context.disableDefaultConstraintViolation();
+        for (PasswordRequirement requirement : requirements) {
+            try {
+                requirement.satisfy(value);
+            } catch (PasswordRequirementException e) {
+                addConstraintViolation(context, e);
+                isValid = false;
+            }
+        }
+        return isValid;
+    }
+
+    private void addConstraintViolation(ConstraintValidatorContext context, PasswordRequirementException e) {
+        String text = e.getText();
+        String template = context.getDefaultConstraintMessageTemplate();
+        String message = template.replace(VerifiedPassword.TEXT_PLACEHOLDER, text);
+        context.buildConstraintViolationWithTemplate(message).addConstraintViolation();
+    }
+
+}
