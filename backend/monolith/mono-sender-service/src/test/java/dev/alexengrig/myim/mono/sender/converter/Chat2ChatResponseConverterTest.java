73d34557c4052feff5901f9b567145d871361a90
Grig Alex <alexengrigdev@gmail.com>
2022-02-15 04:30:51 +0300

    Add converter tests to sender-service

---
+++ backend/monolith/mono-sender-service/src/test/java/dev/alexengrig/myim/mono/sender/converter/Chat2ChatResponseConverterTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2022 Alexengrig Dev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.alexengrig.myim.mono.sender.converter;
+
+import dev.alexengrig.myim.mono.sender.domain.Chat;
+import dev.alexengrig.myim.mono.sender.payload.ChatResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(classes = {Chat2ChatResponseConverterImpl.class})
+class Chat2ChatResponseConverterTest {
+
+    @Autowired
+    Chat2ChatResponseConverter converter;
+
+    @Test
+    void should_convert() {
+        Chat source = Chat.builder()
+                .id("test-id")
+                .name("test-name")
+                .build();
+        ChatResponse actual = converter.convert(source);
+        ChatResponse expected = ChatResponse.builder()
+                .id("test-id")
+                .name("test-name")
+                .build();
+        assertEquals(expected, actual, "ChatResponse");
+    }
+
+}
---
+++ backend/monolith/mono-sender-service/src/test/java/dev/alexengrig/myim/mono/sender/converter/ChatMessageSearchResult2ChatMessageSearchResponseConverterTest.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2022 Alexengrig Dev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.alexengrig.myim.mono.sender.converter;
+
+import dev.alexengrig.myim.mono.sender.config.SenderConversionServiceAdapter;
+import dev.alexengrig.myim.mono.sender.domain.Author;
+import dev.alexengrig.myim.mono.sender.domain.ChatMessage;
+import dev.alexengrig.myim.mono.sender.domain.ChatMessageSearchParams;
+import dev.alexengrig.myim.mono.sender.domain.ChatMessageSearchResult;
+import dev.alexengrig.myim.mono.sender.payload.ChatMessageResponse;
+import dev.alexengrig.myim.mono.sender.payload.ChatMessageSearchResponse;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.support.DefaultConversionService;
+import org.springframework.util.ReflectionUtils;
+
+import java.lang.reflect.Field;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(classes = ChatMessageSearchResult2ChatMessageSearchResponseConverterTest.Config.class)
+class ChatMessageSearchResult2ChatMessageSearchResponseConverterTest {
+
+    @Autowired
+    ChatMessageSearchResult2ChatMessageSearchResponseConverter converter;
+
+    @Test
+    void should_convert() {
+        ChatMessageSearchResult source = ChatMessageSearchResult.builder()
+                .params(ChatMessageSearchParams.builder()
+                        .size(10)
+                        .offset(20)
+                        .chatId("test-chat-id")
+                        .build())
+                .values(Collections.singletonList(
+                        ChatMessage.builder()
+                                .text("test-text")
+                                .chatId("test-chat-id")
+                                .author(Author.builder()
+                                        .id("test-author-id")
+                                        .name("test-author-name")
+                                        .build())
+                                .build()))
+                .total(1000)
+                .build();
+        ChatMessageSearchResponse actual = converter.convert(source);
+        ChatMessageSearchResponse expected = ChatMessageSearchResponse.builder()
+                .values(Collections.singletonList(
+                        ChatMessageResponse.builder()
+                                .text("test-text")
+                                .chatId("test-chat-id")
+                                .authorId("test-author-id")
+                                .authorName("test-author-name")
+                                .build()))
+                .total(1000)
+                .build();
+        assertEquals(expected, actual, "ChatMessageSearchResponse");
+    }
+
+    static class Config {
+
+        @Bean
+        ChatMessage2ChatMessageResponseConverter chatMessage2ChatMessageResponseConverter() {
+            return new ChatMessage2ChatMessageResponseConverterImpl();
+        }
+
+        @Bean
+        ConversionService conversionService(ChatMessage2ChatMessageResponseConverter converter) {
+            DefaultConversionService service = new DefaultConversionService();
+            service.addConverter(converter);
+            return service;
+        }
+
+        @Bean
+        SenderConversionServiceAdapter adapter(ConversionService conversionService) {
+            return new SenderConversionServiceAdapter(conversionService);
+        }
+
+        @Bean
+        @SneakyThrows(NoSuchFieldException.class)
+        ChatMessageSearchResult2ChatMessageSearchResponseConverter converter(SenderConversionServiceAdapter adapter) {
+            var converter = new ChatMessageSearchResult2ChatMessageSearchResponseConverterImpl();
+            var converterType = ChatMessageSearchResult2ChatMessageSearchResponseConverterImpl.class;
+            Field field = converterType.getDeclaredField("senderConversionServiceAdapter");
+            ReflectionUtils.makeAccessible(field);
+            ReflectionUtils.setField(field, converter, adapter);
+            return converter;
+        }
+
+    }
+
+}
