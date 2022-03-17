db6b03a900aa9138c362828ef99d67b95b7a4aa2
Grig Alex <alexengrigdev@gmail.com>
2022-03-18 00:58:34 +0300

    Create UserResponse with converter

---
+++ backend/monolith/mono-security-service/src/main/java/dev/alexengrig/myim/mono/security/converter/ApplicationUserDetails2UserResponseConverter.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2022 Alexengrig Dev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.alexengrig.myim.mono.security.converter;
+
+import dev.alexengrig.myim.mono.security.config.MapStructConfiguration;
+import dev.alexengrig.myim.mono.security.domain.ApplicationUserDetails;
+import dev.alexengrig.myim.mono.security.payload.UserResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.core.convert.converter.Converter;
+
+@Mapper(config = MapStructConfiguration.class)
+public interface ApplicationUserDetails2UserResponseConverter
+        extends Converter<ApplicationUserDetails, UserResponse> {
+
+    @Mapping(target = "id", source = "username")
+    @Mapping(target = "name", source = "username")
+    @Override
+    UserResponse convert(ApplicationUserDetails source);
+
+}
