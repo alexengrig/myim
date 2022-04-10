ab854520782cc22d946057c935b4365eb3768f43
Grig Alex <alexengrigdev@gmail.com>
2022-04-10 20:16:52 +0300

    Add chats creation

--- backend/monolith/mono-runner/src/main/java/dev/alexengrig/myim/mono/listener/DefaultValuesCreationOnApplicationReadyListener.java
+++ backend/monolith/mono-runner/src/main/java/dev/alexengrig/myim/mono/listener/DefaultValuesCreationOnApplicationReadyListener.java
@@ -16,8 +16,11 @@
 
 package dev.alexengrig.myim.mono.listener;
 
+import dev.alexengrig.myim.mono.domain.Chat;
+import dev.alexengrig.myim.mono.manager.service.ChatManagerService;
 import dev.alexengrig.myim.mono.security.domain.ApplicationUserDetails;
 import dev.alexengrig.myim.mono.security.service.ApplicationUserDetailsManager;
+import dev.alexengrig.myim.mono.store.repository.ChatRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -25,6 +28,8 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -39,11 +44,20 @@ public class DefaultValuesCreationOnApplicationReadyListener
     private static final int LAST_USER_NUMBER = 9;
     private static final String USERNAME_PREFIX = "user";
 
+    private static final int FIRST_CHAT_NUMBER = 0;
+    private static final int LAST_CHAT_NUMBER = 9;
+    private static final String CHAT_NAME_PREFIX = "Chat#";
+
     private final ApplicationUserDetailsManager userManager;
+
+    private final ChatRepository chatRepository;
+
+    private final ChatManagerService chatManagerService;
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
         createDefaultUsersIfNeed();
+        createDefaultChatsIfNeed();
     }
 
     private Stream<String> getUsernames() {
@@ -60,7 +74,22 @@ public class DefaultValuesCreationOnApplicationReadyListener
                         .build());
             }
         });
-        log.info("Development users: userX/userX (X = [0-9])");
+        log.info("Development users: {}X/{}X (X = [{}-{}])",
+                USERNAME_PREFIX, USERNAME_PREFIX, FIRST_USER_NUMBER, LAST_USER_NUMBER);
+    }
+
+    private void createDefaultChatsIfNeed() {
+        if (chatRepository.count() == 0) {
+            List<Chat> newChats = IntStream.rangeClosed(FIRST_CHAT_NUMBER, LAST_CHAT_NUMBER)
+                    .mapToObj(i -> CHAT_NAME_PREFIX + i)
+                    .map(name -> Chat.builder()
+                            .name(name)
+                            .build())
+                    .collect(Collectors.toList());
+            chatManagerService.createAll(newChats);
+            log.info("Development chats: {}X (X = [{}-{}])",
+                    CHAT_NAME_PREFIX, FIRST_CHAT_NUMBER, LAST_CHAT_NUMBER);
+        }
     }
 
 }
